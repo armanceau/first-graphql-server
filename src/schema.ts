73459cd125ee80c739a45f37ea344c8775a3e955
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-03-18 17:17:11 +0100

    Update schema.ts

--- src/schema.ts
+++ src/schema.ts
@@ -4,9 +4,9 @@ export const typeDefs = gql`
   type Query {
     doctors(specialities: [Speciality!]): [Doctor]
     doctor(id: ID!): Doctor
-    add(number1: Float!, number2: Float!): Float
-    substract(number1: Float!, number2: Float!): Float
-    multiply(number1: Float!, number2: Float!): Float
+    add(number1: Float!, number2: Float!): Float!
+    substract(number1: Float!, number2: Float!): Float!
+    multiply(number1: Float!, number2: Float!): Float!
     divide(number1: Float!, number2: Float!): Float
     closestColor(color: String!): String
   }

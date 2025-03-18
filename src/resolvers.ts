11d6e9319f94e1a277cf98d1d19e12a19425161e
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-03-18 17:24:30 +0100

    Update resolvers.ts

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -22,11 +22,14 @@ export const resolvers = {
     },
     divide: (_, { number1, number2 }) => {
       if (number2 === 0) {
-        throw new GraphQLError('Impossible de diviser par 0')
+        throw new GraphQLError("Impossible de diviser par 0");
       }
       return number1 / number2;
     },
-    closestColor: (_, { color }) => {
+    closestColor: (_, { color }: { color: string }) => {
+      if (!color.match(/^#[0-9a-fA-F]{6}/)) {
+        throw new GraphQLError("Couleur invalide");
+      }
       return closestColor(color, colorsData);
     },
   },
